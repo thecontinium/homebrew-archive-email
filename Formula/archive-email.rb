a84f77e5f13d92ab16ccaba96995b9180bc8e06d
The Continium <thecontinium@outlook.com>
2014-10-31 12:44:38 +1100

    .

--- Formula/archive-email.rb
+++ Formula/archive-email.rb
@@ -28,7 +28,6 @@ class ArchiveEmail < Formula
       <key>ProgramArguments</key>
       <array>
         <string>/usr/local/bin/archive-email</string>
-        <string>#{prefix}</string>
       </array>
       <key>StandardErrorPath</key>
       <string>/tmp/#{plist_name}.err</string>
